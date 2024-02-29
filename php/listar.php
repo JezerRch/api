9d25ee91b27d0641a96356d770b8c20957657728
JezerRch <jezerprocha@hotmail.com>
2024-02-29 08:30:19 -0400

    fisrt-commit

---
+++ php/listar.php
@@ -0,0 +1,29 @@
+<?php
+
+//Incluir a conexao
+include('conexao.php');
+
+//SQL
+$sql = "SELECT * FROM cursos";
+
+//Executar
+$executar = mysqli_query($conexao, $sql);
+
+//Vetor
+$cursos = [];
+
+//Indice
+$indice = 0;
+
+//Laço
+while ($linha = mysqli_fetch_assoc($executar)) {
+    $cursos[$indice]['idCurso'] = $linha['idCurso'];
+    $cursos[$indice]['nomeCurso'] = $linha['nomeCurso'];
+    $cursos[$indice]['valorCurso'] = $linha['valorCurso'];
+    $indice++;
+}
+
+//JSON
+json_encode(['cursos' => $cursos]);
+
+// var_dump($cursos);
